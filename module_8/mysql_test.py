033c2269d290118785cb3a623092890bcfb6f273
Patrick Loyd <patrickloyd789@gmail.com>
2022-11-25 10:22:50 -0500

    Pysports Setup

---
+++ module_8/mysql_test.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from mysql.connector import errorcode
+
+"""
+    Title: pysports_join_queries.py
+    Author: Patrick Loyd
+    Date: 25 Nov 2022
+    Description: Test program for joining the player and team tables
+"""
+
+""" import statements """
+
+""" database config object """
+config = {
+    "user": "pysports_user",
+    "password": "MySQL8IsGreat!",
+    "host": "127.0.0.1",
+    "database": "pysports",
+    "raise_on_warnings": True
+}
+
+try:
+    """ try/catch block for handling potential MySQL database errors """
+
+    db = mysql.connector.connect(**config)
+    # output the connection status
+    print("\n  Database user {} connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n  Press any key to continue...")
+
+except mysql.connector.Error as err:
+    """ on error code """
+
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("  The supplied username or password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("  The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    """ close the connection to MySQL """
+
+    db.close()
